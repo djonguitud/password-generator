55564b95ea98ca6bea8a388f6842fdb2b7ad2ab0
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-10-01 16:34:39 -0500

    chore: add random password all characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
-// Código de asignación
+/* 
 var generateBtn = document.querySelector("#generate");
 
-// Escriba la contraseña en la entrada #password
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +10,108 @@ function writePassword() {
 
 }
 
-// Agregar oyente de eventos para generar el botón
 generateBtn.addEventListener("click", writePassword);
+ */
+
+let tempArr = [];
+let password;
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const capLett = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'M',
+	'N',
+	'O',
+	'p',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
+
+const lowLett = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
+
+const espChar = [
+	'"',
+	'!',
+	'"',
+	'#',
+	'$',
+	'%',
+	'&',
+	"'",
+	'(',
+	')',
+	'*',
+	'+',
+	'-',
+	'.',
+	'/',
+	':',
+	';',
+	'<',
+	'>',
+	'?',
+	'@',
+	'[',
+	'\\',
+	']',
+	'^',
+	'_',
+	'{',
+	'|',
+	'}',
+	'~',
+];
+
+const passLen = Number(prompt('Seleccione el largo de la contraseña entre 8 y 128 carácteres'));
+
+let concatArr = [...numbers, ...capLett, ...lowLett, ...espChar];
+
+for (let i = 0; i < passLen; i++) {
+	tempArr.push(concatArr[Math.floor(Math.random() * (concatArr.length + 1))]);
+}
+
+console.log(tempArr);
+8;
